3252708c6be361f4e1013c152133d163abaf35ae
Tristan Williams <aenemenate@gmail.com>
2020-09-13 23:58:10 -0700

    change: remove doall.bat, add debug.bat debug.bat opens the executable w/ visual studio

---
+++ debug.bat
@@ -0,0 +1,7 @@
+@echo off
+
+@echo .
+@echo Opening debug environment for Aztheria and Elendor: Worlds of Dragons
+cd bin
+devenv A-and-E-Worlds-of-Dragons.exe
+cd ../
--- doall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@echo .
-@echo Starting build
-call build
-call run
