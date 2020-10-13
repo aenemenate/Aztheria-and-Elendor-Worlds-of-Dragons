0a636bba6465ef02a716ff6c600becc9835a582b
Tristan Williams <aenemenate@gmail.com>
2020-10-13 00:52:31 -0700

    prettify: output of build.bat, update README.md remove CMake.md

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 I initially started developing this game so my sister could play about dragons, since she is so obsessed with them.
 
-Read CMake.md for instructions on building if you are unfamiliar (courtesy to williamkeller).
+To build simply run bin/build (currently works on mac and windows)
--- bin/build.bat
+++ bin/build.bat
@@ -10,6 +10,7 @@ IF NOT EXIST build mkdir build
 
 cd build
 cmake ..
+echo .
 IF "%1" == "" msbuild aztheria_and_elendor.sln /p:Configuration=Release
 IF "%1" == "/release" msbuild aztheria_and_elendor.sln /p:Configuration=Release
 IF "%1" == "/debug" msbuild aztheria_and_elendor.sln /p:Configuration=Debug
