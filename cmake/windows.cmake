e008514e26de5f138d3f90c336ffd6e9fdaf8272
William Keller <william@arda.net>
2020-10-12 20:21:10 -0600

    Missed linker flags for windows

--- cmake/windows.cmake
+++ cmake/windows.cmake
@@ -1,7 +1,7 @@
 
 
 # Compiler setup
-SET(CMAKE_CXX_FLAGS "/std:c++17 /EHsc")
+SET(CMAKE_CXX_FLAGS "/c /std:c++17 /Z7 /MT /O2")
 
 # Linker setup
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /SUBSYSTEM:WINDOWS") 
